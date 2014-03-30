491bbed372af85d6ff2d54864218346d8c278afc
Nikhil Sri Ram <nikhilsriram05@gmail.com>
2014-03-30 17:45:17 +0530

    Bullets added. Could not compress the spring beforehand

--- src/dominos.cpp
+++ src/dominos.cpp
@@ -182,8 +182,17 @@ namespace cs296
       fd2->restitution = 0.f;
       fd2->shape = new b2EdgeShape;
       b2EdgeShape bs2;
-      bs2.Set(b2Vec2(-30.f, 4.f), b2Vec2(0.f, 4.f));
+      bs2.Set(b2Vec2(-30.f, 4.f), b2Vec2(-1.f, 4.f));
       fd2->shape = &bs2;
+      
+      b2FixtureDef *fd22 = new b2FixtureDef;
+      fd22->density = 10.0;
+      fd22->friction = 0.5;
+      fd22->restitution = 0.f;
+      fd22->shape = new b2EdgeShape;
+      b2EdgeShape bs22;
+      bs22.Set(b2Vec2(-1.f, 4.f), b2Vec2(0.f, 3.f));
+      fd22->shape = &bs22;
       
       b2FixtureDef *fd3 = new b2FixtureDef;
       fd3->density = 10.0;
@@ -201,7 +210,7 @@ namespace cs296
       fd4->restitution = 0.f;
       fd4->shape = new b2PolygonShape;
       b2PolygonShape bs4;
-      bs4.SetAsBox(0.2,2, b2Vec2(-8.f,-2.f), 0);
+      bs4.SetAsBox(0.2,1.9, b2Vec2(-8.f,-2.f), 0);
       fd4->shape = &bs4;
       
       b2FixtureDef *fd5 = new b2FixtureDef;
@@ -219,7 +228,7 @@ namespace cs296
       fd6->restitution = 0.f;
       fd6->shape = new b2PolygonShape;
       b2PolygonShape bs6;
-      bs6.SetAsBox(0.2,7.5, b2Vec2(0.f,-7.5f), 0);
+      bs6.SetAsBox(0.2,9.0, b2Vec2(0.f,-5.75f), 0);
       fd6->shape = &bs6;
       
       b2FixtureDef *fd7 = new b2FixtureDef;
@@ -257,10 +266,24 @@ namespace cs296
       b2EdgeShape bs10;
       bs10.Set(b2Vec2(-30.f, 4.f), b2Vec2(-30.f, 3.f));
       fd10->shape = &bs10;
+      
+      b2FixtureDef *fd11 = new b2FixtureDef;
+      fd11->density = 10.0;
+      fd11->friction = 0.5;
+      fd11->restitution = 0.f;
+      fd11->shape = new b2PolygonShape;
+      b2PolygonShape bs11;
+      b2Vec2 vert[3];
+      vert[0].Set(8.f,0.f);
+      vert[1].Set(8.f,3.9f);
+      vert[2].Set(11.f,3.9f);
+      bs11.Set(vert,3);
+      fd11->shape = &bs11;
        
       b2Body* box1 = m_world->CreateBody(bd);
       box1->CreateFixture(fd1);
       box1->CreateFixture(fd2);
+      box1->CreateFixture(fd22);
       box1->CreateFixture(fd3);
       box1->CreateFixture(fd4);
       box1->CreateFixture(fd5);
@@ -269,6 +292,7 @@ namespace cs296
       box1->CreateFixture(fd8);
       box1->CreateFixture(fd9);
       box1->CreateFixture(fd10);
+      box1->CreateFixture(fd11);
   }
   
   /** dynamic part
@@ -280,7 +304,7 @@ namespace cs296
       bd->fixedRotation = true;
       
       b2FixtureDef *fd1 = new b2FixtureDef;
-      fd1->density = 10.0;
+      fd1->density = 50.0;
       fd1->friction = 0;
       fd1->restitution = 1.f;
       fd1->shape = new b2PolygonShape;
@@ -288,12 +312,129 @@ namespace cs296
       bs1.SetAsBox(4,2, b2Vec2(-26.f,2.f), 0);
       fd1->shape = &bs1;
        
+      b2FixtureDef *fd2 = new b2FixtureDef;
+      fd2->density = 50.0;
+      fd2->friction = 0;
+      fd2->restitution = 1.f;
+      fd2->shape = new b2PolygonShape;
+      b2PolygonShape bs2;
+      bs2.SetAsBox(11,0.2, b2Vec2(-19.f,7.2f), 0);
+      fd2->shape = &bs2;
+      
+      b2FixtureDef *fd3 = new b2FixtureDef;
+      fd3->density = 50.0;
+      fd3->friction = 0;
+      fd3->restitution = 1.f;
+      fd3->shape = new b2PolygonShape;
+      b2PolygonShape bs3;
+      bs3.SetAsBox(5,1.5, b2Vec2(5.f,5.5f), 0);
+      fd3->shape = &bs3;
+      
+      b2BodyDef *invibd = new b2BodyDef;
+      invibd->type = b2_staticBody;
+      invibd->position.Set(0-6,20+2);
+      invibd->fixedRotation = true;
+      b2Body* invi = m_world->CreateBody(invibd);
+      
       b2Body* box1 = m_world->CreateBody(bd);
       box1->CreateFixture(fd1);
+      box1->CreateFixture(fd2);
+      box1->CreateFixture(fd3);
       box1->SetLinearVelocity(b2Vec2(3,0));
       
+      b2DistanceJointDef spring1;
+      spring1.Initialize(box1, invi, b2Vec2(-22,22), invi->GetWorldCenter());
+      spring1.collideConnected=true;
+      spring1.frequencyHz = 0.04f;
+      spring1.dampingRatio = 0.0f;
+      m_world->CreateJoint(&spring1);
+        
      }
-	   
+	 
+	 /**bullet spring part
+	  * */
+	  
+	{
+	  b2BodyDef *invibd = new b2BodyDef;
+      invibd->type = b2_staticBody;
+      invibd->position.Set(0+4,20-15);
+      invibd->fixedRotation = true;
+      b2Body* invi = m_world->CreateBody(invibd);
+      
+	  b2BodyDef *bd = new b2BodyDef;
+      bd->type = b2_dynamicBody;
+      bd->position.Set(0,20);
+      bd->fixedRotation = true;
+      
+      b2FixtureDef *fd1 = new b2FixtureDef;
+      fd1->density = 10.0;
+      fd1->friction = 0;
+      fd1->restitution = 1.f;
+      fd1->shape = new b2PolygonShape;
+      b2PolygonShape bs1;
+      bs1.SetAsBox(3.9,0.2, b2Vec2(4.f,0.f), 0);
+      fd1->shape = &bs1;
+       
+     /* b2FixtureDef *fd2 = new b2FixtureDef;
+      fd2->density = 10.0;
+      fd2->friction = 0;
+      fd2->restitution = 1.f;
+      fd2->shape = new b2PolygonShape;
+      b2PolygonShape bs2;
+      bs2.SetAsBox(11,0.2, b2Vec2(-19.f,7.2f), 0);
+      fd2->shape = &bs2;
+      
+      b2FixtureDef *fd3 = new b2FixtureDef;
+      fd3->density = 10.0;
+      fd3->friction = 0;
+      fd3->restitution = 1.f;
+      fd3->shape = new b2PolygonShape;
+      b2PolygonShape bs3;
+      bs3.SetAsBox(5,1.5, b2Vec2(5.f,5.5f), 0);
+      fd3->shape = &bs3;*/
+        
+      b2Body* box1 = m_world->CreateBody(bd);
+      box1->CreateFixture(fd1);
+      //box1->CreateFixture(fd2);
+      //box1->CreateFixture(fd3);
+      box1->SetLinearVelocity(b2Vec2(0,3));
+      
+      b2DistanceJointDef spring1;
+      spring1.Initialize(box1, invi, box1->GetWorldCenter(), invi->GetWorldCenter());
+      spring1.collideConnected=true;
+      spring1.frequencyHz = 0.85f;
+      spring1.dampingRatio = 0.0f;
+      m_world->CreateJoint(&spring1);
+  }
+  //bullet part
+  {
+	      b2FixtureDef *bf = new b2FixtureDef;
+		  bf->density = 5.0;
+		  bf->friction = 0.0;
+		  bf->restitution = 0.0f;
+		  bf->shape = new b2PolygonShape;
+		  b2PolygonShape bs;
+		  b2Vec2 vert[5];
+		  vert[0].Set(0.f,0.f);
+		  vert[1].Set(1.f,1.4f);
+		  vert[2].Set(6.f,1.4f);
+		  vert[3].Set(6.f,-1.4f);
+		  vert[4].Set(1.f,-1.4f);
+		  bs.Set(vert,5);
+		  bf->shape = &bs;
+		  
+	  for(int i=0; i<6 ;i++){
+		  b2BodyDef *bulletDef = new b2BodyDef;
+          bulletDef->type = b2_dynamicBody;
+          bulletDef->position.Set(0,26-2*i);
+          bulletDef->fixedRotation = true;
+      
+
+		  b2Body* bullet = m_world->CreateBody(bulletDef);
+		  bullet->CreateFixture(bf);
+	}
+	}
+		  
 	   
 	   
   /*
