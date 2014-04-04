80f11d4b7e8429b58ec75a2d64d026db3f4016ac
SAICHARAN <saicharanduppati@gmail.com>
2014-04-04 14:16:21 +0530

    shell added

---
+++ external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Collision/Shapes/b2ChainShape.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2ChainShape.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Collision/Shapes/b2CircleShape.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2CircleShape.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Collision/Shapes/b2EdgeShape.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2EdgeShape.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Collision/Shapes/b2PolygonShape.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/Shapes/b2PolygonShape.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Collision/b2BroadPhase.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2BroadPhase.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Collision/b2CollideCircle.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2CollideCircle.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Collision/b2CollideEdge.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2CollideEdge.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Collision/b2CollidePolygon.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2CollidePolygon.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Collision/b2Collision.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2Collision.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Collision/b2Distance.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2Distance.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Collision/b2DynamicTree.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2DynamicTree.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Collision/b2TimeOfImpact.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Collision/b2TimeOfImpact.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Common/b2BlockAllocator.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Common/b2BlockAllocator.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Common/b2Draw.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Common/b2Draw.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Common/b2Math.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Common/b2Math.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Common/b2Settings.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Common/b2Settings.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Common/b2StackAllocator.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Common/b2StackAllocator.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Common/b2Timer.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Common/b2Timer.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Contacts/b2ChainAndCircleContact.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2ChainAndCircleContact.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Contacts/b2ChainAndPolygonContact.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2ChainAndPolygonContact.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Contacts/b2CircleContact.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2CircleContact.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Contacts/b2Contact.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2Contact.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Contacts/b2ContactSolver.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2ContactSolver.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Contacts/b2EdgeAndCircleContact.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2EdgeAndCircleContact.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Contacts/b2EdgeAndPolygonContact.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2EdgeAndPolygonContact.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Contacts/b2PolygonAndCircleContact.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2PolygonAndCircleContact.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Contacts/b2PolygonContact.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Contacts/b2PolygonContact.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Joints/b2DistanceJoint.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2DistanceJoint.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Joints/b2FrictionJoint.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2FrictionJoint.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Joints/b2GearJoint.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2GearJoint.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Joints/b2Joint.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2Joint.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Joints/b2MotorJoint.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2MotorJoint.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Joints/b2MouseJoint.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2MouseJoint.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Joints/b2PrismaticJoint.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2PrismaticJoint.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Joints/b2PulleyJoint.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2PulleyJoint.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Joints/b2RevoluteJoint.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2RevoluteJoint.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Joints/b2RopeJoint.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2RopeJoint.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Joints/b2WeldJoint.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2WeldJoint.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/Joints/b2WheelJoint.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/Joints/b2WheelJoint.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/b2Body.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2Body.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/b2ContactManager.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2ContactManager.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/b2Fixture.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2Fixture.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/b2Island.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2Island.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/b2World.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2World.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Dynamics/b2WorldCallbacks.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Dynamics/b2WorldCallbacks.cpp.o"
+  "/home/saicharan/Desktop/gun/external/src/Box2D/Box2D/Rope/b2Rope.cpp" "/home/saicharan/Desktop/gun/external/src/Box2D/build296/Box2D/CMakeFiles/Box2D.dir/Rope/b2Rope.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Box2D/.."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- src/dominos.cpp
+++ src/dominos.cpp
@@ -45,118 +45,8 @@ namespace cs296
   
   dominos_t::dominos_t()
   {
-    /*//Ground
-    /! \var b1 
-     * \brief pointer to the body ground 
-      
-    b2Body* b1;  
-    {
-      
-      b2EdgeShape shape; 
-      shape.Set(b2Vec2(-90.0f, 0.0f), b2Vec2(90.0f, 0.0f));
-      b2BodyDef bd; 
-      b1 = m_world->CreateBody(&bd); 
-      b1->CreateFixture(&shape, 0.0f);
-    }
-          
-    //Top horizontal shelf
-    {
-      b2PolygonShape shape;
-      shape.SetAsBox(6.0f, 0.25f);
-	
-      b2BodyDef bd;
-      bd.position.Set(-31.0f, 30.0f);
-      b2Body* ground = m_world->CreateBody(&bd);
-      ground->CreateFixture(&shape, 0.0f);
-    }
+    
 
-    //Dominos
-    {
-      b2PolygonShape shape;
-      shape.SetAsBox(0.1f, 1.0f);
-	
-      b2FixtureDef fd;
-      fd.shape = &shape;
-      fd.density = 20.0f;
-      fd.friction = 0.1f;
-		
-      for (int i = 0; i < 10; ++i)
-	{
-	  b2BodyDef bd;
-	  bd.type = b2_dynamicBody;
-	  bd.position.Set(-35.5f + 1.0f * i, 31.25f);
-	  b2Body* body = m_world->CreateBody(&bd);
-	  body->CreateFixture(&fd);
-	}
-    }
-      
-    //Another horizontal shelf
-    {
-      b2PolygonShape shape;
-      shape.SetAsBox(7.0f, 0.25f, b2Vec2(-20.f,20.f), 0.0f);
-	
-      b2BodyDef bd;
-      bd.position.Set(1.0f, 6.0f);
-      b2Body* ground = m_world->CreateBody(&bd);
-      ground->CreateFixture(&shape, 0.0f);
-    }
-
-
-    //The pendulum that knocks the dominos off
-    {
-      b2Body* b2;
-      {
-	b2PolygonShape shape;
-	shape.SetAsBox(0.25f, 1.5f);
-	  
-	b2BodyDef bd;
-	bd.position.Set(-36.5f, 28.0f);
-	b2 = m_world->CreateBody(&bd);
-	b2->CreateFixture(&shape, 10.0f);
-      }
-	
-      b2Body* b4;
-      {
-	b2PolygonShape shape;
-	shape.SetAsBox(0.25f, 0.25f);
-	  
-	b2BodyDef bd;
-	bd.type = b2_dynamicBody;
-	bd.position.Set(-40.0f, 33.0f);
-	b4 = m_world->CreateBody(&bd);
-	b4->CreateFixture(&shape, 2.0f);
-      }
-	
-      b2RevoluteJointDef jd;
-      b2Vec2 anchor;
-      anchor.Set(-37.0f, 40.0f);
-      jd.Initialize(b2, b4, anchor);
-      m_world->CreateJoint(&jd);
-    }
-      
-    //The train of small spheres
-    {
-      b2Body* spherebody;
-	
-      b2CircleShape circle;
-      circle.m_radius = 0.5;
-	
-      b2FixtureDef ballfd;
-      ballfd.shape = &circle;
-      ballfd.density = 1.0f;
-      ballfd.friction = 0.0f;
-      ballfd.restitution = 0.0f;
-	
-      for (int i = 0; i < 10; ++i)
-	{
-	  b2BodyDef ballbd;
-	  ballbd.type = b2_dynamicBody;
-	  ballbd.position.Set(-22.2f + i*1.0, 26.6f);
-	  spherebody = m_world->CreateBody(&ballbd);
-	  spherebody->CreateFixture(&ballfd);
-	}
-    }
-*/
     /**Skeleton (static system)
      * */
     {
@@ -175,6 +65,16 @@ namespace cs296
       b2EdgeShape bs1;
       bs1.Set(b2Vec2(-30.f, 7.f), b2Vec2(0.f,7.f));
       fd1->shape = &bs1;
+      
+      b2FixtureDef *window = new b2FixtureDef;
+      (*window).filter.groupIndex=-1;
+      window->density = 10.0;
+      window->friction = 0.5;
+      window->restitution = 0.f;
+      window->shape = new b2EdgeShape;
+      b2EdgeShape windows;
+      windows.Set(b2Vec2(0.f, 7.f), b2Vec2(8.f,7.f));
+      window->shape = &windows;
       
       b2FixtureDef *fd2 = new b2FixtureDef;
       fd2->density = 10.0;
@@ -237,7 +137,7 @@ namespace cs296
       fd7->restitution = 0.f;
       fd7->shape = new b2PolygonShape;
       b2PolygonShape bs7;
-      bs7.SetAsBox(4,0.2, b2Vec2(4.0f,-15.f), 0);
+      bs7.SetAsBox(3.7,0.2, b2Vec2(3.7f,-15.f), 0);
       fd7->shape = &bs7;
       
       b2FixtureDef *fd8 = new b2FixtureDef;
@@ -246,7 +146,7 @@ namespace cs296
       fd8->restitution = 0.f;
       fd8->shape = new b2PolygonShape;
       b2PolygonShape bs8;
-      bs8.SetAsBox(0.2,7.5, b2Vec2(8.f,-7.5f), 0);
+      bs8.SetAsBox(0.2,7.5, b2Vec2(8.0f,-7.5f), 0);
       fd8->shape = &bs8;
       
       b2FixtureDef *fd9 = new b2FixtureDef;
@@ -274,14 +174,15 @@ namespace cs296
       fd11->shape = new b2PolygonShape;
       b2PolygonShape bs11;
       b2Vec2 vert[3];
-      vert[0].Set(8.f,0.f);
-      vert[1].Set(8.f,3.9f);
+      vert[0].Set(7.8f,0.f);
+      vert[1].Set(7.8f,3.9f);
       vert[2].Set(11.f,3.9f);
       bs11.Set(vert,3);
       fd11->shape = &bs11;
        
       b2Body* box1 = m_world->CreateBody(bd);
       box1->CreateFixture(fd1);
+      box1->CreateFixture(window);
       box1->CreateFixture(fd2);
       box1->CreateFixture(fd22);
       box1->CreateFixture(fd3);
@@ -309,7 +210,7 @@ namespace cs296
       fd1->restitution = 1.f;
       fd1->shape = new b2PolygonShape;
       b2PolygonShape bs1;
-      bs1.SetAsBox(4,2, b2Vec2(-26.f,2.f), 0);
+      bs1.SetAsBox(4,1.85, b2Vec2(-26.f,2.f), 0);
       fd1->shape = &bs1;
        
       b2FixtureDef *fd2 = new b2FixtureDef;
@@ -327,7 +228,7 @@ namespace cs296
       fd3->restitution = 1.f;
       fd3->shape = new b2PolygonShape;
       b2PolygonShape bs3;
-      bs3.SetAsBox(5,1.5, b2Vec2(5.f,5.5f), 0);
+      bs3.SetAsBox(5,1.4, b2Vec2(5.f,5.4f), 0);
       fd3->shape = &bs3;
       
       b2BodyDef *invibd = new b2BodyDef;
@@ -350,7 +251,79 @@ namespace cs296
       m_world->CreateJoint(&spring1);
         
      }
-	 
+     //bullets
+	   {
+	      //shell
+		  b2FixtureDef *sf = new b2FixtureDef;
+		  sf->density = 10.0;
+		  sf->friction = 0.5;
+		  sf->restitution = 0.f;
+		  b2Vec2 vs[8];
+          vs[0].Set(3.5f, 1.0f);
+          vs[1].Set(0.0f, 1.0f);
+	      vs[2].Set(0.3f, 1.3f);
+		  vs[3].Set(5.2f, 1.3f);
+		  vs[4].Set(5.2f, -1.3f);
+		  vs[5].Set(0.3f, -1.3f);
+		  vs[6].Set(0.0f, -1.0f);
+		  vs[7].Set(3.5f, -1.0f);
+		  b2ChainShape chain;
+          chain.CreateChain(vs, 8);
+          sf->shape = &chain;
+          		 
+	      
+	      b2FixtureDef *bf = new b2FixtureDef;
+		  bf->density = 5.0;
+		  bf->friction = 0.0;
+		  bf->restitution = 0.0f;
+		  bf->shape = new b2PolygonShape;
+		  b2PolygonShape bs;
+		  b2Vec2 vert[5];
+		  vert[0].Set(0.f,0.f);
+		  vert[1].Set(1.f,1.f);
+		  vert[2].Set(6.f,1.f);
+		  vert[3].Set(6.f,-1.f);
+		  vert[4].Set(1.f,-1.f);
+		  bs.Set(vert,5);
+		  bf->shape = &bs;
+		  
+	  for(int i=0; i<6 ;i++){
+		  b2BodyDef *bulletDef = new b2BodyDef;
+          (*bf).filter.groupIndex=i;
+          bulletDef->type = b2_dynamicBody;
+          bulletDef->position.Set(0.4,24-2.6*i);
+          bulletDef->fixedRotation = true;
+          
+          b2Body* bullet = m_world->CreateBody(bulletDef);
+		  bullet->CreateFixture(bf);
+		  
+		  b2BodyDef *shellDef = new b2BodyDef;
+          shellDef->type = b2_dynamicBody;
+          shellDef->position.Set(1.9,25-2.6*i);
+          (*sf).filter.groupIndex=-1;
+          b2Body* shell = m_world->CreateBody(shellDef);
+		  shell->CreateFixture(sf);
+		  
+		  b2DistanceJointDef stuff;
+		  stuff.Initialize(bullet, shell, bullet->GetWorldCenter(), shell->GetWorldCenter());
+		  stuff.collideConnected=true;
+		  stuff.frequencyHz = 10.f;
+		  m_world->CreateJoint(&stuff);
+		  
+      
+		  
+		  
+	}
+		  b2BodyDef *bulletDef = new b2BodyDef;
+          bulletDef->type = b2_dynamicBody;
+          bulletDef->position.Set(0,-20);
+          bulletDef->fixedRotation = true;
+          
+          b2Body* bullet = m_world->CreateBody(bulletDef);
+		  bullet->CreateFixture(bf);
+      
+	}
+	
 	 /**bullet spring part
 	  * */
 	  
@@ -372,192 +345,22 @@ namespace cs296
       fd1->restitution = 1.f;
       fd1->shape = new b2PolygonShape;
       b2PolygonShape bs1;
-      bs1.SetAsBox(3.9,0.2, b2Vec2(4.f,0.f), 0);
+      bs1.SetAsBox(3.2,0.2, b2Vec2(3.8f,-10.f), 0);
       fd1->shape = &bs1;
        
-     /* b2FixtureDef *fd2 = new b2FixtureDef;
-      fd2->density = 10.0;
-      fd2->friction = 0;
-      fd2->restitution = 1.f;
-      fd2->shape = new b2PolygonShape;
-      b2PolygonShape bs2;
-      bs2.SetAsBox(11,0.2, b2Vec2(-19.f,7.2f), 0);
-      fd2->shape = &bs2;
-      
-      b2FixtureDef *fd3 = new b2FixtureDef;
-      fd3->density = 10.0;
-      fd3->friction = 0;
-      fd3->restitution = 1.f;
-      fd3->shape = new b2PolygonShape;
-      b2PolygonShape bs3;
-      bs3.SetAsBox(5,1.5, b2Vec2(5.f,5.5f), 0);
-      fd3->shape = &bs3;*/
-        
       b2Body* box1 = m_world->CreateBody(bd);
       box1->CreateFixture(fd1);
-      //box1->CreateFixture(fd2);
-      //box1->CreateFixture(fd3);
       box1->SetLinearVelocity(b2Vec2(0,3));
       
       b2DistanceJointDef spring1;
       spring1.Initialize(box1, invi, box1->GetWorldCenter(), invi->GetWorldCenter());
       spring1.collideConnected=true;
-      spring1.frequencyHz = 0.85f;
+      spring1.frequencyHz = 1.1f;
       spring1.dampingRatio = 0.0f;
+      spring1.length = 15.0f;
       m_world->CreateJoint(&spring1);
   }
-  //bullet part
-  {
-	      b2FixtureDef *bf = new b2FixtureDef;
-		  bf->density = 5.0;
-		  bf->friction = 0.0;
-		  bf->restitution = 0.0f;
-		  bf->shape = new b2PolygonShape;
-		  b2PolygonShape bs;
-		  b2Vec2 vert[5];
-		  vert[0].Set(0.f,0.f);
-		  vert[1].Set(1.f,1.4f);
-		  vert[2].Set(6.f,1.4f);
-		  vert[3].Set(6.f,-1.4f);
-		  vert[4].Set(1.f,-1.4f);
-		  bs.Set(vert,5);
-		  bf->shape = &bs;
-		  
-	  for(int i=0; i<6 ;i++){
-		  b2BodyDef *bulletDef = new b2BodyDef;
-          bulletDef->type = b2_dynamicBody;
-          bulletDef->position.Set(0,26-2*i);
-          bulletDef->fixedRotation = true;
-      
-
-		  b2Body* bullet = m_world->CreateBody(bulletDef);
-		  bullet->CreateFixture(bf);
-	}
-	}
-		  
-	   
-	   
-  /*
-
-      //The bar
-      bd->position.Set(10,15);	
-      fd1->density = 34.0;	  
-      b2Body* box2 = m_world->CreateBody(bd);
-      box2->CreateFixture(fd1);
-
-      // The pulley joint
-      b2PulleyJointDef* myjoint = new b2PulleyJointDef();
-      b2Vec2 worldAnchorOnBody1(-10, 15); // Anchor point on body 1 in world axis
-      b2Vec2 worldAnchorOnBody2(10, 15); // Anchor point on body 2 in world axis
-      b2Vec2 worldAnchorGround1(-10, 20); // Anchor point for ground 1 in world axis
-      b2Vec2 worldAnchorGround2(10, 20); // Anchor point for ground 2 in world axis
-      float32 ratio = 1.0f; // Define ratio
-      myjoint->Initialize(box1, box2, worldAnchorGround1, worldAnchorGround2, box1->GetWorldCenter(), box2->GetWorldCenter(), ratio);
-      m_world->CreateJoint(myjoint);
-    }
-
-    //The revolving horizontal platform
-    {
-      b2PolygonShape shape;
-      shape.SetAsBox(2.2f, 0.2f);
-	
-      b2BodyDef bd;
-      bd.position.Set(14.0f, 14.0f);
-      bd.type = b2_dynamicBody;
-      b2Body* body = m_world->CreateBody(&bd);
-      b2FixtureDef *fd = new b2FixtureDef;
-      fd->density = 1.f;
-      fd->shape = new b2PolygonShape;
-      fd->shape = &shape;
-      body->CreateFixture(fd);
-
-      b2PolygonShape shape2;
-      shape2.SetAsBox(0.2f, 2.0f);
-      b2BodyDef bd2;
-      bd2.position.Set(14.0f, 16.0f);
-      b2Body* body2 = m_world->CreateBody(&bd2);
-
-      b2RevoluteJointDef jointDef;
-      jointDef.bodyA = body;
-      jointDef.bodyB = body2;
-      jointDef.localAnchorA.Set(0,0);
-      jointDef.localAnchorB.Set(0,0);
-      jointDef.collideConnected = false;
-      m_world->CreateJoint(&jointDef);
-    }
-
-    //The heavy sphere on the platform
-    {
-      b2Body* sbody;
-      b2CircleShape circle;
-      circle.m_radius = 1.0;
-	
-      b2FixtureDef ballfd;
-      ballfd.shape = &circle;
-      ballfd.density = 50.0f;
-      ballfd.friction = 0.0f;
-      ballfd.restitution = 0.0f;
-      b2BodyDef ballbd;
-      ballbd.type = b2_dynamicBody;
-      ballbd.position.Set(14.0f, 18.0f);
-      sbody = m_world->CreateBody(&ballbd);
-      sbody->CreateFixture(&ballfd);
-    }
-
-
-    //The see-saw system at the bottom
-    {
-      //The triangle wedge
-      b2Body* sbody;
-      b2PolygonShape poly;
-      b2Vec2 vertices[3];
-      vertices[0].Set(-1,0);
-      vertices[1].Set(1,0);
-      vertices[2].Set(0,1.5);
-      poly.Set(vertices, 3);
-      b2FixtureDef wedgefd;
-      wedgefd.shape = &poly;
-      wedgefd.density = 10.0f;
-      wedgefd.friction = 0.0f;
-      wedgefd.restitution = 0.0f;
-      b2BodyDef wedgebd;
-      wedgebd.position.Set(30.0f, 0.0f);
-      sbody = m_world->CreateBody(&wedgebd);
-      sbody->CreateFixture(&wedgefd);
-
-      //The plank on top of the wedge
-      b2PolygonShape shape;
-      shape.SetAsBox(15.0f, 0.2f);
-      b2BodyDef bd2;
-      bd2.position.Set(30.0f, 1.5f);
-      bd2.type = b2_dynamicBody;
-      b2Body* body = m_world->CreateBody(&bd2);
-      b2FixtureDef *fd2 = new b2FixtureDef;
-      fd2->density = 1.f;
-      fd2->shape = new b2PolygonShape;
-      fd2->shape = &shape;
-      body->CreateFixture(fd2);
-
-      b2RevoluteJointDef jd;
-      b2Vec2 anchor;
-      anchor.Set(30.0f, 1.5f);
-      jd.Initialize(sbody, body, anchor);
-      m_world->CreateJoint(&jd);
-
-      //The light box on the right side of the see-saw
-      b2PolygonShape shape2;
-      shape2.SetAsBox(2.0f, 2.0f);
-      b2BodyDef bd3;
-      bd3.position.Set(40.0f, 2.0f);
-      bd3.type = b2_dynamicBody;
-      b2Body* body3 = m_world->CreateBody(&bd3);
-      b2FixtureDef *fd3 = new b2FixtureDef;
-      fd3->density = 0.01f;
-      fd3->shape = new b2PolygonShape;
-      fd3->shape = &shape2;
-      body3->CreateFixture(fd3);
-    }*/
-  }
+ }
 
   sim_t *sim = new sim_t("Dominos", dominos_t::create);
 }
